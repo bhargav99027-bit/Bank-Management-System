06ffec4c0eccf3ed8adc736a00b2982cf01116aa
Bhargav hv <bhargav99027@gmail.com>
2026-08-27 19:00:21 +0530

    Add main menu

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+    cout<<"--------------------------------------------------------------------------------------------"<<endl;
+    cout<<"=================================BANK MANAGEMENT SYSTEM====================================="<<endl;
+    cout<<"--------------------------------------------------------------------------------------------"<<endl;
+    cout<<"1--> CREATE ACCOUNT"<<endl;
+    cout<<"2--> DISPLAY ACCOUNTS"<<endl;
+    cout<<"3--> SEARCHING ACCOUNT"<<endl;
+    cout<<"4--> UPDATE ACCOUNT"<<endl;
+    cout<<"5--> DELETE ACCOUNT"<<endl;
+    cout<<"6--> TRANSACTIONS"<<endl;
+    cout<<"5--> SAVE ACCOUNT"<<endl;
+    cout<<"6--> DISPLAY MENU AGAIN"<<endl;
+    cout<<"0--> EXIT"<<endl; 
+}
