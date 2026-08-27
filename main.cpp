e27d0a6b94dad6a189737281c299186a4450dd4d
Bhargav hv <bhargav99027@gmail.com>
2026-08-27 19:16:33 +0530

    Add switch-case menu handling

--- main.cpp
+++ main.cpp
@@ -14,7 +14,73 @@ int main()
     cout<<"4--> UPDATE ACCOUNT"<<endl;
     cout<<"5--> DELETE ACCOUNT"<<endl;
     cout<<"6--> TRANSACTIONS"<<endl;
-    cout<<"5--> SAVE ACCOUNT"<<endl;
-    cout<<"6--> DISPLAY MENU AGAIN"<<endl;
+    cout<<"7--> SAVE ACCOUNT"<<endl;
     cout<<"0--> EXIT"<<endl; 
+
+
+    while(1)
+    {
+    int  choice;
+    cout<<"---------------------------------------------------------------------------------------------"<<endl;
+    cout<<"ENTER YOUR CHOICE: ";
+    cin>>choice;
+    switch (choice)
+    {
+
+    case 1:
+    {
+        cout<<"Development under progress";
+        break;
+    }
+
+    case 2:
+    {
+       cout<<"Development under progress";
+       break;
+    }
+
+    case 3:
+    {
+        cout<<"Development under progress";
+        break;
+    }
+
+    case 4:
+    {
+        cout<<"Development under progress";
+        break;
+    }
+
+    case 5:
+    {
+        cout<<"Development under progress";
+        break;
+    }
+    
+    case 6:
+    {
+        cout<<"Development under progress";  
+        break;
+    }
+
+    case 7:
+    {
+        cout<<"Development under progress";  
+        break;
+    }
+
+    case 0:
+    {
+        cout<<"EXITED FROM MANAGMENT"<<endl;
+        return 0;
+    }
+    
+    default:
+    {
+        cout<<"INVALID STATE, PLEASE TRY WHICH ARE IN MENU"<<endl;
+        break;
+    }
+  }
+
+ }
 }
